86bf6e81adc1f0c6662c1896790febe9bbf155eb
Samuel Rizzon <shrizzon@gmail.com>
2025-03-18 09:51:47 -0300

    ✨ feat: add badge component documentation and demos (#39)
    * ✨ feat: add badge component documentation and demos
    
    * 🚧 chore: add concurrently package and update start script
    
    * 📦 refactor: make badge component standalone

--- libs/zard/src/lib/components/components.ts
+++ libs/zard/src/lib/components/components.ts
@@ -1,3 +1,5 @@
-export * from './input/input.directive';
+export * from './badge/badge.component';
 export * from './button/button.component';
+export * from './card/card.component';
 export * from './checkbox/checkbox.component';
+export * from './input/input.directive';
