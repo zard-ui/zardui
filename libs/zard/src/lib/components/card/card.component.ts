5ef1c6157773192ac188c8dd734e5d9b4974a49a
Henrique Custódia <martinshenrique8@gmail.com>
2025-03-17 21:18:14 -0300

    feat(card): add header and body components

--- libs/zard/src/lib/components/card/card.component.ts
+++ libs/zard/src/lib/components/card/card.component.ts
@@ -3,42 +3,75 @@ import { ClassValue } from 'class-variance-authority/dist/types';
 import { ChangeDetectionStrategy, Component, computed, input, ViewEncapsulation } from '@angular/core';
 
 import { mergeClasses } from '../../shared/utils/utils';
-import { cardBodyVariants, cardHeaderVariants, cardVariants } from './card.variants';
+import { cardBodyVariants, cardHeaderDescriptionVariants, cardHeaderTitleVariants, cardHeaderVariants, cardVariants } from './card.variants';
+
+@Component({
+  selector: 'z-card-body',
+  exportAs: 'zCardBody',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class]': 'classes()',
+  },
+})
+export class ZardCardBodyComponent {
+  protected readonly classes = computed(() => mergeClasses(cardBodyVariants()));
+}
+
+@Component({
+  selector: 'z-card-header',
+  exportAs: 'zCardHeader',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class]': 'classes()',
+  },
+})
+export class ZardCardHeaderComponent {
+  readonly class = input<ClassValue>('');
+  protected readonly classes = computed(() => mergeClasses(cardHeaderVariants(), this.class()));
+}
+
+@Component({
+  selector: 'z-card-header-title',
+  exportAs: 'zCardHeaderTitle',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class]': 'classes()',
+  },
+})
+export class ZardCardHeaderTitleComponent {
+  readonly class = input<ClassValue>('');
+  protected readonly classes = computed(() => mergeClasses(cardHeaderTitleVariants(), this.class()));
+}
+
+@Component({
+  selector: 'z-card-header-description',
+  exportAs: 'zCardHeaderDescription',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '<ng-content></ng-content>',
+  host: {
+    '[class]': 'classes()',
+  },
+})
+export class ZardCardHeaderDescriptionComponent {
+  readonly class = input<ClassValue>('');
+  protected readonly classes = computed(() => mergeClasses(cardHeaderDescriptionVariants(), this.class()));
+}
 
 @Component({
   selector: 'z-card',
   exportAs: 'zCard',
-  standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
-  template: `
-    @if (zTitle()) {
-      <div [class]="headerClasses()">
-        <div class="text-2xl font-semibold leading-none tracking-tight">{{ zTitle() }}</div>
-
-        @if (zDescription()) {
-          <div class="text-sm text-muted-foreground">
-            {{ zDescription() }}
-          </div>
-        }
-      </div>
-    }
-
-    <div [class]="bodyClasses()">
-      <ng-content></ng-content>
-    </div>
-  `,
+  template: `<ng-content></ng-content>`,
   host: {
     '[class]': 'classes()',
   },
 })
 export class ZardCardComponent {
-  readonly zTitle = input<string>();
-  readonly zDescription = input<string>();
-
   readonly class = input<ClassValue>('');
 
   protected readonly classes = computed(() => mergeClasses(cardVariants(), this.class()));
-  protected readonly headerClasses = computed(() => mergeClasses(cardHeaderVariants()));
-  protected readonly bodyClasses = computed(() => mergeClasses(cardBodyVariants()));
 }
--- libs/zard/src/lib/components/card/card.variants.ts
+++ libs/zard/src/lib/components/card/card.variants.ts
@@ -1,16 +1,26 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
-export const cardVariants = cva('block rounded-lg border bg-card text-card-foreground shadow-sm w-full', {
+export const cardVariants = cva('block rounded-lg border bg-card text-card-foreground shadow-sm w-full p-6', {
   variants: {},
 });
 export type ZardCardVariants = VariantProps<typeof cardVariants>;
 
-export const cardHeaderVariants = cva('flex flex-col space-y-1.5 p-6 pb-0 gap-1.5', {
+export const cardHeaderVariants = cva('flex flex-col space-y-1.5 pb-0 gap-1.5', {
   variants: {},
 });
 export type ZardCardHeaderVariants = VariantProps<typeof cardHeaderVariants>;
 
-export const cardBodyVariants = cva('p-6', {
+export const cardHeaderTitleVariants = cva('text-2xl font-semibold leading-none tracking-tight', {
+  variants: {},
+});
+export type ZardCardHeaderTitleVariants = VariantProps<typeof cardHeaderTitleVariants>;
+
+export const cardHeaderDescriptionVariants = cva('text-sm text-muted-foreground', {
+  variants: {},
+});
+export type ZardCardHeaderDescriptionVariants = VariantProps<typeof cardHeaderDescriptionVariants>;
+
+export const cardBodyVariants = cva('block mt-6', {
   variants: {},
 });
 export type ZardCardBodyVariants = VariantProps<typeof cardBodyVariants>;
