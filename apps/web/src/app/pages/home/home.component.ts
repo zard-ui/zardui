b4bb08a4d7e9e8d58a11b48fef65711786f0c35d
Luiz Gomes <luizgomes77777@gmail.com>
2025-03-19 16:03:02 -0300

    ✨ feat: implementing new sections for the home page

--- apps/web/src/app/pages/home/home.component.ts
+++ apps/web/src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
-import { ZardButtonComponent } from '@zard/components/button/button.component';
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
+
+import { FeaturesComponent } from '../../domain/components/components/features/features.component';
+import { YoutubeComponent } from '../../domain/components/components/youtube/youtube.component';
+import { HeroComponent } from '../../domain/components/components/hero/hero.component';
 
 @Component({
   selector: 'z-home',
   standalone: true,
-  imports: [RouterModule, ZardButtonComponent],
-  templateUrl: './home.component.html',
+  imports: [RouterModule, HeroComponent, FeaturesComponent, YoutubeComponent],
+  template: `
+    <z-hero></z-hero>
+    <z-features></z-features>
+    <z-youtube></z-youtube>
+  `,
 })
 export class HomeComponent {}
