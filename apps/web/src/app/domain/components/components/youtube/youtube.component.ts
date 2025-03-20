6b196c2d11381872a695b186c3131ac4815cab7f
Luiz Gomes <luizgomes77777@gmail.com>
2025-03-19 22:00:01 -0300

    ✨ feat: updating home sections for the project alpha

---
+++ apps/web/src/app/domain/components/components/youtube/youtube.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'z-youtube',
+  standalone: true,
+  template: `
+    <section class="min-h-screen max-w-4xl mx-auto mt-24">
+      <header>
+        <h1 class="text-center text-3xl xl:text-6xl font-semibold mb-8">Featured on YouTube</h1>
+      </header>
+
+      <main class="grid grid-cols-1 gap-6 w-full">
+        <section class="relative h-full overflow-hidden rounded-xl border bg-secondary shadow-sm">
+          <iframe
+            width="100%"
+            height="600"
+            src="https://www.youtube.com/embed/o4Xjn-EZ1Lo?si=0vkbdH_XEl9VeXWK"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </section>
+      </main>
+    </section>
+  `,
+})
+export class YoutubeComponent {}
--- apps/web/src/app/pages/home/home.component.ts
+++ apps/web/src/app/pages/home/home.component.ts
@@ -1,11 +1,17 @@
-import { ZardButtonComponent } from '@zard/components/button/button.component';
+import { ShowcaseComponent } from '@zard/domain/components/components/showcase/showcase.component';
+import { YoutubeComponent } from '@zard/domain/components/components/youtube/youtube.component';
+import { HeroComponent } from '@zard/domain/components/components/hero/hero.component';
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'z-home',
   standalone: true,
-  imports: [RouterModule, ZardButtonComponent],
-  templateUrl: './home.component.html',
+  imports: [RouterModule, HeroComponent, ShowcaseComponent, YoutubeComponent],
+  template: `
+    <z-hero></z-hero>
+    <z-showcase></z-showcase>
+    <z-youtube></z-youtube>
+  `,
 })
 export class HomeComponent {}
