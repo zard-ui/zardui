13c7a5da6dd753073076362e14e6777e0d738c25
David Vilaça <vilacapdavid@gmail.com>
2025-03-21 16:32:05 -0300

    ✨ feat(radio): add radio component

--- apps/web/src/app/shared/constants/components.constant.ts
+++ apps/web/src/app/shared/constants/components.constant.ts
@@ -1,24 +1,27 @@
-import { ComponentType } from '@angular/cdk/overlay';
-import { ZardDemoBadgeBasicComponent } from '@zard/components/badge/demo/basic';
-import { ZardDemoBadgeShapeComponent } from '@zard/components/badge/demo/shape';
-import { ZardDemoButtonBasicComponent } from '@zard/components/button/demo/basic';
-import { ZardDemoButtonFullComponent } from '@zard/components/button/demo/full';
-import { ZardDemoButtonLoadingComponent } from '@zard/components/button/demo/loading';
-import { ZardDemoButtonShapeComponent } from '@zard/components/button/demo/shape';
-import { ZardDemoButtonSizeComponent } from '@zard/components/button/demo/size';
-import { ZardDemoCardBasicComponent } from '@zard/components/card/demo/basic';
-import { ZardDemoCheckboxBasicComponent } from '@zard/components/checkbox/demo/basic';
 import { ZardDemoCheckboxDisabledComponent } from '@zard/components/checkbox/demo/disabled';
-import { ZardDemoCheckboxShapeComponent } from '@zard/components/checkbox/demo/shape';
-import { ZardDemoCheckboxSizeComponent } from '@zard/components/checkbox/demo/size';
-import { ZardDemoInputBasicComponent } from '@zard/components/input/demo/basic';
 import { ZardDemoInputBorderlessComponent } from '@zard/components/input/demo/borderless';
-import { ZardDemoInputSizeComponent } from '@zard/components/input/demo/size';
-import { ZardDemoInputStatusComponent } from '@zard/components/input/demo/status';
-import { ZardDemoInputTextAreaComponent } from '@zard/components/input/demo/text-area';
-import { ZardDemoSwitchBasicComponent } from '@zard/components/switch/demo/basic';
 import { ZardDemoSwitchDisabledComponent } from '@zard/components/switch/demo/disabled';
+import { ZardDemoInputTextAreaComponent } from '@zard/components/input/demo/text-area';
+import { ZardDemoRadioDisabledComponent } from '@zard/components/radio/demo/disabled';
+import { ZardDemoCheckboxShapeComponent } from '@zard/components/checkbox/demo/shape';
+import { ZardDemoCheckboxBasicComponent } from '@zard/components/checkbox/demo/basic';
+import { ZardDemoButtonLoadingComponent } from '@zard/components/button/demo/loading';
+import { ZardDemoCheckboxSizeComponent } from '@zard/components/checkbox/demo/size';
+import { ZardDemoSwitchBasicComponent } from '@zard/components/switch/demo/basic';
+import { ZardDemoInputStatusComponent } from '@zard/components/input/demo/status';
+import { ZardDemoButtonShapeComponent } from '@zard/components/button/demo/shape';
+import { ZardDemoButtonBasicComponent } from '@zard/components/button/demo/basic';
 import { ZardDemoSwitchSizeComponent } from '@zard/components/switch/demo/size';
+import { ZardDemoRadioBasicComponent } from '@zard/components/radio/demo/basic';
+import { ZardDemoInputBasicComponent } from '@zard/components/input/demo/basic';
+import { ZardDemoButtonSizeComponent } from '@zard/components/button/demo/size';
+import { ZardDemoButtonFullComponent } from '@zard/components/button/demo/full';
+import { ZardDemoBadgeShapeComponent } from '@zard/components/badge/demo/shape';
+import { ZardDemoBadgeBasicComponent } from '@zard/components/badge/demo/basic';
+import { ZardDemoRadioSizeComponent } from '@zard/components/radio/demo/size';
+import { ZardDemoInputSizeComponent } from '@zard/components/input/demo/size';
+import { ZardDemoCardBasicComponent } from '@zard/components/card/demo/basic';
+import { ComponentType } from '@angular/cdk/overlay';
 
 export interface ComponentData {
   componentName: string;
@@ -139,6 +142,23 @@ export const COMPONENTS: ComponentData[] = [
       {
         name: 'disabled',
         component: ZardDemoSwitchDisabledComponent,
+      },
+    ],
+  },
+  {
+    componentName: 'radio',
+    examples: [
+      {
+        name: 'basic',
+        component: ZardDemoRadioBasicComponent,
+      },
+      {
+        name: 'size',
+        component: ZardDemoRadioSizeComponent,
+      },
+      {
+        name: 'disabled',
+        component: ZardDemoRadioDisabledComponent,
       },
     ],
   },
