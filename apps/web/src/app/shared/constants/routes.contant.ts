bf7c5c1af8d661d21c99c33f92cf7e0df4dece68
Luiz Gomes <luizgomes77777@gmail.com>
2025-03-24 20:03:49 -0300

    ✨ feat: updating sidebar to adapt to the application's docs and components standard

--- apps/web/src/app/shared/constants/routes.contant.ts
+++ apps/web/src/app/shared/constants/routes.contant.ts
@@ -32,29 +32,42 @@ export const HEADER_PATHS = [
   },
 ];
 
-export const SIDEBAR_PATHS = [
-  {
-    name: 'Badge',
-    path: '/components/badge',
-  },
-  {
-    name: 'Button',
-    path: '/components/button',
-  },
-  {
-    name: 'Card',
-    path: '/components/card',
-  },
-  {
-    name: 'Checkbox',
-    path: '/components/checkbox',
-  },
-  {
-    name: 'Input',
-    path: '/components/input',
-  },
-  {
-    name: 'Switch',
-    path: '/components/switch',
-  },
-];
+const DOCS_PATH = {
+  title: 'Getting Started',
+  data: [
+    { name: 'introduction', path: '/docs', available: false },
+    { name: 'installation', path: '/docs/installation', available: true },
+    { name: 'theming', path: '/docs', available: false },
+    { name: 'cli', path: '/docs', available: false },
+    { name: 'figma', path: '/docs', available: false },
+  ],
+};
+
+const COMPONENTS_PATH = {
+  title: 'Components',
+  data: [
+    { name: 'Accordion', path: '/components', available: false },
+    { name: 'Alert', path: '/components', available: false },
+    { name: 'Avatar', path: '/components', available: false },
+    { name: 'Badge', path: '/components/badge', available: true },
+    { name: 'Breadcrumb', path: '/components', available: false },
+    { name: 'Button', path: '/components/button', available: true },
+    { name: 'Calendar', path: '/components', available: false },
+    { name: 'Card', path: '/components/card', available: true },
+    { name: 'Carousel', path: '/components', available: false },
+    { name: 'Chart', path: '/components', available: false },
+    { name: 'Checkbox', path: '/components/checkbox', available: true },
+    { name: 'Confirm Modal', path: '/components', available: false },
+    { name: 'Input', path: '/components/input', available: true },
+    { name: 'Modal', path: '/components', available: false },
+    { name: 'Progress Bar', path: '/components', available: false },
+    { name: 'Select', path: '/components', available: false },
+    { name: 'Switch', path: '/components/switch', available: true },
+    { name: 'Table', path: '/components', available: false },
+    { name: 'Tabs', path: '/components', available: false },
+    { name: 'Toast', path: '/components', available: false },
+    { name: 'Tooltip', path: '/components', available: false },
+  ].sort((a, b) => a.name.localeCompare(b.name)),
+};
+
+export const SIDEBAR_PATHS = [DOCS_PATH, COMPONENTS_PATH];
