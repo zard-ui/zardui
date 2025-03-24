95cd8d988de51bdf82b99eb8f573ab969898c52c
Luiz Gomes <luizgomes77777@gmail.com>
2025-03-24 20:37:45 -0300

    ✨ feat: adding max width to the documentation content and making archor cleaner

--- apps/web/src/app/domain/components/footer/footer.component.ts
+++ apps/web/src/app/domain/components/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   selector: 'z-footer',
   template: `
     <footer class="w-full border-t bg-background/95">
-      <main class="max-w-[90rem] h-16 mx-auto px-4 flex items-center justify-center xl:justify-start">
+      <main class="max-w-[90rem] h-16 mx-auto px-4 md:px-6 flex items-center justify-center xl:justify-start">
         <p class="text-muted-foreground text-sm">
           Made with ❤️ from 🇧🇷 | Open source and available on
           <a class="font-medium underline underline-offset-4" [href]="githubUrl" target="_blank">Github</a>.
--- apps/web/src/app/shared/constants/routes.contant.ts
+++ apps/web/src/app/shared/constants/routes.contant.ts
@@ -2,34 +2,10 @@ export const DEFAULT_COMPONENT = 'badge';
 export const DEFAULT_DOC = 'installation';
 
 export const HEADER_PATHS = [
-  {
-    name: 'Docs',
-    path: '/docs',
-    available: true,
-    activeClass: '!text-foreground font-semibold',
-    class: 'transition-colors hover:text-foreground/80 text-foreground/80 gap-2',
-  },
-  {
-    name: 'Components',
-    path: '/components',
-    available: true,
-    activeClass: '!text-foreground font-semibold',
-    class: 'transition-colors hover:text-foreground/80 text-foreground/80 gap-2',
-  },
-  {
-    name: 'Blocks',
-    path: '/blocks',
-    available: false,
-    activeClass: '!text-foreground font-semibold',
-    class: 'transition-colors hover:text-foreground/80 text-foreground/80 gap-2',
-  },
-  {
-    name: 'Themes',
-    path: '/themes',
-    available: false,
-    activeClass: '!text-foreground font-semibold',
-    class: 'transition-colors hover:text-foreground/80 text-foreground/80 gap-2',
-  },
+  { name: 'Docs', path: '/docs', available: true },
+  { name: 'Components', path: '/components', available: true },
+  { name: 'Blocks', path: '/blocks', available: false },
+  { name: 'Themes', path: '/themes', available: false },
 ];
 
 const DOCS_PATH = {
