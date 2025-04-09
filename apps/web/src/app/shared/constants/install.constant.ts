85b498b4012dd6dd69b8fece041dcd222de7e701
Luiz gomes <62315802+Luizgomess@users.noreply.github.com>
2025-04-09 02:19:57 -0300

    Feat/#45 automatic documentation generation v2 (#82)
    * ✨ feat: updating prismjs theme
    
    * ✨ feat: custom clipboard button for code blocks
    
    * ✨ feat: updating markdown design (components, doc)
    
    * 🐛 fix: removing nested components folders in domain folder
    
    * ✨ feat: updating doc pages architecture
    
    * ✨ feat: first concept of docs page
    
    * ✨ feat: updating sidebar to adapt to the application's docs and components standard
    
    * ✨ feat: changing standard classes to utility classes
    
    * 🐛 fix: adjusting route active class in the header
    
    * ✨ feat: adding max width to the documentation content and making archor cleaner
    
    * ✨ feat: updating api table design
    
    * ✨ feat: updating the badge component document to the new standard
    
    * ✨ feat: updating the button component document to the new standard
    
    * 🐛 fix: fixing card component doc generation
    
    * ✨ feat: updating the checkbox component document to the new standard
    
    * ✨ feat: updating the input component document to the new standard
    
    * ✨ feat: updating the loader component document to the new standard
    
    * ✨ feat: updating the switch component document to the new standard
    
    * ✨ feat: updating the radio component document to the new standard
    
    * ✨ feat: updating the card component document to the new standard
    
    * 🚧 chore: remove unused files
    
    * ✨ feat: add installation and environments pages with step-by-step guides
    
    * ✨ feat: update home route to use correct path and add HomePage component
    
    * ✨ feat: update styles to use CSS instead of SCSS and reorganize project configuration
    
    * 📦 refactor: enhance zard-code-box layout and improve tab transition handling
    
    * ✨ feat: update installation guide for Angular with detailed CLI instructions and correct description
    
    * 🐛 fix: remove unnecessary whitespace from buttonVariants string
    
    * 🚧 chore: re-add ZardDemoRadioDestructiveComponent and remove outdated demo markdown files

--- apps/web/src/app/shared/constants/install.constant.ts
+++ apps/web/src/app/shared/constants/install.constant.ts
@@ -22,7 +22,7 @@ export const installations: Installation[] = [
   {
     environment: 'angular',
     title: 'Angular',
-    description: 'Install and configure shadcn/ui for Next.js.',
+    description: 'Install and configure zard/ui for Angular.',
     manual: [
       {
         title: 'Create project',
@@ -73,6 +73,55 @@ export const installations: Installation[] = [
         subtitle: 'You can now start adding components to your project.',
       },
     ],
-    cli: [],
+    cli: [
+      {
+        title: 'Create project',
+        subtitle: 'Start the cli and create an application that uses CSS as default styling.',
+        url: {
+          text: "If you don't know, you can follow the initial documentation step by step",
+          href: 'https://angular.dev/installation',
+          external: true,
+        },
+        file: '/angular/install.md',
+      },
+      {
+        title: 'Add Tailwind CSS',
+        subtitle: 'Components are styled using Tailwind CSS. You need to install Tailwind CSS in your project.',
+        url: {
+          text: 'Follow the Tailwind CSS installation instructions to get started.',
+          href: 'https://tailwindcss.com/docs/installation/framework-guides/angular',
+          external: true,
+        },
+      },
+      {
+        title: 'Add dependencies',
+        subtitle: 'Add the following dependencies to your project:',
+        file: '/angular/dependencies.md',
+      },
+      {
+        title: 'Configure path aliases',
+        subtitle: 'Configure the path aliases in your tsconfig.json file.',
+        file: '/angular/tsconfig.md',
+      },
+      {
+        title: 'Configure styles',
+        subtitle: 'Add the following to your styles.css file. You can learn more about using CSS variables for theming in the',
+        url: {
+          text: 'theming section.',
+          href: '/docs/theming',
+        },
+        file: '/angular/theming.md',
+      },
+      {
+        title: 'Add a cn helper',
+        subtitle: 'Create a folder called lib inside your app folder',
+        path: 'lib/utils.ts',
+        file: '/angular/helper.md',
+      },
+      {
+        title: "That's it",
+        subtitle: 'You can now start adding components to your project.',
+      },
+    ],
   },
 ];
--- libs/zard/src/lib/components/switch/demo/size.md
+++
@@ -1,3 +0,0 @@
-**Size**
-
-Zard supports a default switch size, as well as a small and large size. These sizes can be applied to the switch using the `zSize` directive.
