50533d315b12271856d1d024d19ddfb40c06c51c
devmatheusribeiro <github.matheusribeiro@gmail.com>
2025-08-05 22:36:17 -0300

    ✨ feat: add skeleton component

--- apps/web/src/app/shared/constants/components.constant.ts
+++ apps/web/src/app/shared/constants/components.constant.ts
@@ -1,20 +1,26 @@
+import { TOGGLE_GROUP } from '@zard/components/toggle-group/demo/toggle-group';
 import { PROGRESS_BAR } from '@zard/components/progress-bar/demo/progress-bar';
+import { DATE_PICKER } from '@zard/components/date-picker/demo/date-picker';
 import { BREADCRUMB } from '@zard/components/breadcrumb/demo/breadcrumb';
 import { ACCORDION } from '@zard/components/accordion/demo/accordion';
+import { SKELETON } from '@zard/components/skeleton/demo/skeleton';
 import { DROPDOWN } from '@zard/components/dropdown/demo/dropdown';
 import { CHECKBOX } from '@zard/components/checkbox/demo/checkbox';
+import { CALENDAR } from '@zard/components/calendar/demo/calendar';
 import { TOOLTIP } from '@zard/components/tooltip/demo/tooltip';
+import { POPOVER } from '@zard/components/popover/demo/popover';
 import { DIVIDER } from '@zard/components/divider/demo/divider';
+import { COMMAND } from '@zard/components/command/demo/command';
+import { TOGGLE } from '@zard/components/toggle/demo/toggle';
 import { SWITCH } from '@zard/components/switch/demo/switch';
 import { SLIDER } from '@zard/components/slider/demo/slider';
-import { TABLE } from '@zard/components/table/demo/table';
 import { SELECT } from '@zard/components/select/demo/select';
 import { LOADER } from '@zard/components/loader/demo/loader';
 import { DIALOG } from '@zard/components/dialog/demo/dialog';
 import { BUTTON } from '@zard/components/button/demo/button';
-import { CALENDAR } from '@zard/components/calendar/demo/calendar';
 import { AVATAR } from '@zard/components/avatar/demo/avatar';
-import { DATE_PICKER } from '@zard/components/date-picker/demo/date-picker';
+import { TOAST } from '@zard/components/toast/demo/toast';
+import { TABLE } from '@zard/components/table/demo/table';
 import { RADIO } from '@zard/components/radio/demo/radio';
 import { INPUT } from '@zard/components/input/demo/input';
 import { BADGE } from '@zard/components/badge/demo/badge';
@@ -22,11 +28,6 @@ import { ALERT } from '@zard/components/alert/demo/alert';
 import { TABS } from '@zard/components/tabs/demo/tabs';
 import { CARD } from '@zard/components/card/demo/card';
 import { ComponentType } from '@angular/cdk/overlay';
-import { TOGGLE } from '@zard/components/toggle/demo/toggle';
-import { TOGGLE_GROUP } from '@zard/components/toggle-group/demo/toggle-group';
-import { POPOVER } from '@zard/components/popover/demo/popover';
-import { COMMAND } from '@zard/components/command/demo/command';
-import { TOAST } from '@zard/components/toast/demo/toast';
 
 export interface ComponentData {
   componentName: string;
@@ -64,6 +65,7 @@ export const COMPONENTS: ComponentData[] = [
   RADIO,
   SELECT,
   SLIDER,
+  SKELETON,
   SWITCH,
   TABLE,
   TABS,
