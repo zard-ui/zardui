57afff90a47f4957041197e1c2fd04dd0699dea1
ekram70 <ekramullah70@gmail.com>
2025-09-27 10:35:42 +0600

    🔧 chore: update custom style

--- libs/zard/src/lib/components/empty/demo/custom-style.ts
+++ libs/zard/src/lib/components/empty/demo/custom-style.ts
@@ -4,6 +4,6 @@ import { ZardEmptyComponent } from '../empty.component';
 @Component({
   standalone: true,
   imports: [ZardEmptyComponent],
-  template: ` <z-empty class="rounded-xl bg-[#fdfdfd] shadow-[0_4px_12px_rgba(0,0,0,0.08)] p-4" /> `,
+  template: ` <z-empty class="rounded-xl p-4 shadow-[0_4px_12px_rgba(0,0,0,0.08)] dark:shadow-[0_4px_12px_rgba(255,255,255,0.1)]" /> `,
 })
 export class ZardDemoEmptyCustomStyleComponent {}
