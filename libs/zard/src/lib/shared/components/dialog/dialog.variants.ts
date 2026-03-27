355d5eb12d2feaf4888a2a7d5deb4ce51cd9e1ad
viniciuscigma <viniciuscigma@gmail.com>
2026-03-27 00:09:02 -0300

    🐛 fix(dialog): remove hardcoded max-width for dialog variants (#503)

--- libs/zard/src/lib/shared/components/dialog/dialog.variants.ts
+++ libs/zard/src/lib/shared/components/dialog/dialog.variants.ts
@@ -1,6 +1,6 @@
 import { cva, type VariantProps } from 'class-variance-authority';
 
 export const dialogVariants = cva(
-  'fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg rounded-lg max-w-[calc(100%-2rem)] sm:max-w-[425px]',
+  'fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg rounded-lg max-w-[calc(100%-2rem)]',
 );
 export type ZardDialogVariants = VariantProps<typeof dialogVariants>;
