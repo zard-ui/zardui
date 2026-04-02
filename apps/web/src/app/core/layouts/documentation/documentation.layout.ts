2142b64d9445bd663af945401cd27c6cc9adf0cc
Luiz Gomes <gomesluiz.dev@gmail.com>
2026-04-02 20:39:25 -0300

    💄 style(web): refine docs layout spacing and sidebar structure

--- apps/web/src/app/core/layouts/documentation/documentation.layout.ts
+++ apps/web/src/app/core/layouts/documentation/documentation.layout.ts
@@ -12,10 +12,10 @@ import { DarkModeOptions, ZardDarkMode } from '@zard/services/dark-mode';
   template: `
     <main class="container-wrapper flex flex-1 flex-col px-2">
       <div
-        class="group/sidebar-wrapper 3xl:fixed:container 3xl:fixed:px-3 flex min-h-min w-full flex-1 items-start px-0 [--sidebar-width:220px] [--top-spacing:0] lg:grid lg:grid-cols-[var(--sidebar-width)_minmax(0,1fr)] lg:[--sidebar-width:240px] lg:[--top-spacing:calc(var(--spacing)*4)]"
+        class="3xl:fixed:container 3xl:fixed:px-3 min-h-min flex-1 items-start px-0 [--top-spacing:0] lg:grid lg:grid-cols-[var(--sidebar-width)_minmax(0,1fr)] lg:[--top-spacing:calc(var(--spacing)*4)]"
       >
         <z-sidebar></z-sidebar>
-        <section class="mt-4 h-full w-full">
+        <section class="h-full w-full">
           <router-outlet></router-outlet>
         </section>
       </div>
--- apps/web/src/app/domain/components/sidebar/sidebar.component.ts
+++ apps/web/src/app/domain/components/sidebar/sidebar.component.ts
@@ -11,15 +11,23 @@ import { ScrollOnActiveDirective } from './scroll-on-active.directive';
   selector: 'z-sidebar',
   template: `
     <aside
-      class="sticky top-[calc(var(--header-height)+1px)] z-30 hidden h-[calc(100svh-var(--footer-height)-4rem)] overscroll-none bg-transparent lg:flex"
+      class="text-sidebar-foreground sticky top-[calc(var(--header-height)+0.6rem)] z-30 hidden h-[calc(100svh-10rem)] w-(--sidebar-width) flex-col overscroll-none bg-transparent [--sidebar-menu-width:--spacing(56)] lg:flex"
     >
-      <nav class="no-scrollbar flex min-h-0 flex-1 flex-col gap-2 overflow-auto overflow-x-hidden px-2">
-        <div
-          class="from-background via-background/80 to-background/50 pointer-events-none sticky -top-1 z-10 h-2 shrink-0 bg-linear-to-b blur-xs"
-        ></div>
+      <div class="h-9"></div>
+      <div
+        class="from-background via-background/80 to-background/50 absolute top-8 z-10 h-8 w-(--sidebar-menu-width) shrink-0 bg-linear-to-b blur-xs"
+      ></div>
+      <div
+        class="via-border absolute top-12 right-2 bottom-0 hidden h-full w-px bg-linear-to-b from-transparent to-transparent lg:flex"
+      ></div>
+      <nav
+        class="no-scrollbar mx-auto flex min-h-0 w-(--sidebar-menu-width) flex-1 flex-col gap-2 overflow-auto overflow-x-hidden px-2 group-data-[collapsible=icon]:overflow-hidden"
+      >
         @for (doc of sidebarPaths; track $index) {
-          <div class="relative flex w-full min-w-0 flex-col p-2">
-            <h2 class="text-muted-foreground flex h-8 shrink-0 items-center px-2 text-xs font-medium">
+          <div class="relative flex w-full min-w-0 flex-col p-2 pt-6">
+            <h2
+              class="ring-sidebar-ring text-muted-foreground flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium outline-hidden transition-[margin,opacity] duration-200 ease-linear group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0 focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0"
+            >
               {{ doc.title }}
             </h2>
             <div class="w-full text-sm">
@@ -29,7 +37,7 @@ import { ScrollOnActiveDirective } from './scroll-on-active.directive';
                     @if (path.available) {
                       <a
                         [routerLink]="path.path"
-                        class="ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground active:bg-sidebar-accent active:text-sidebar-accent-foreground 3xl:fixed:w-full 3xl:fixed:max-w-48 relative flex h-[30px] w-fit items-center gap-2 overflow-hidden rounded-md border border-transparent px-2 text-left text-[0.8rem] font-medium outline-hidden transition-[width,height,padding] after:absolute after:inset-x-0 after:-inset-y-1 after:z-0 after:rounded-md focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50"
+                        class="ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground active:bg-sidebar-accent active:text-sidebar-accent-foreground 3xl:fixed:w-full 3xl:fixed:max-w-48 relative flex h-7.5 w-fit items-center gap-2 overflow-hidden rounded-md border border-transparent px-2 text-left text-[0.8rem] font-medium outline-hidden transition-[width,height,padding] after:absolute after:inset-x-0 after:-inset-y-1 after:z-0 after:rounded-md focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50"
                         routerLinkActive="bg-accent text-accent-foreground border-accent overflow-visible hover:bg-accent"
                         [routerLinkActiveOptions]="{ exact: true }"
                         scrollOnActive
@@ -39,7 +47,7 @@ import { ScrollOnActiveDirective } from './scroll-on-active.directive';
                       </a>
                     } @else {
                       <span
-                        class="ring-sidebar-ring 3xl:fixed:w-full 3xl:fixed:max-w-48 relative flex h-[30px] w-fit cursor-not-allowed items-center gap-2 overflow-hidden rounded-md border border-transparent px-2 text-left text-[0.8rem] font-medium outline-hidden transition-[width,height,padding] after:absolute after:inset-x-0 after:-inset-y-1 after:z-0 after:rounded-md disabled:pointer-events-none disabled:opacity-50"
+                        class="ring-sidebar-ring 3xl:fixed:w-full 3xl:fixed:max-w-48 relative flex h-7.5 w-fit cursor-not-allowed items-center gap-2 overflow-hidden rounded-md border border-transparent px-2 text-left text-[0.8rem] font-medium outline-hidden transition-[width,height,padding] after:absolute after:inset-x-0 after:-inset-y-1 after:z-0 after:rounded-md disabled:pointer-events-none disabled:opacity-50"
                       >
                         <span class="absolute inset-0 flex w-(--sidebar-width) bg-transparent"></span>
                         {{ path.name }}
@@ -58,9 +66,7 @@ import { ScrollOnActiveDirective } from './scroll-on-active.directive';
       </nav>
     </aside>
   `,
-  host: {
-    class: 'z-30 sticky top-20',
-  },
+
   standalone: true,
   imports: [RouterModule, ZardBadgeComponent, ScrollOnActiveDirective],
 })
